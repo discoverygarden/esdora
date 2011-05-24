4dbe488ebef07c52d23239d85caad6a75b859e91
Jerry Pan <jerry.ypan@gmail.com>
2011-05-24 10:05:28 -0400

    for new atomic content model

---
+++ ajaxtabs_atomic.js
@@ -0,0 +1,65 @@
+var pid;
+var ajaxUrl;
+$(document).ready(function(){
+	  pid = $('#pid').val();
+	  ajaxUrl=Drupal.settings.basePath;
+	  ajaxUrl=ajaxUrl.concat("ajax/gettabs/atomic");
+	  //alert(ajaxUrl);
+	  $('.Items').click(function(){
+		 $('#div1').html('loading...');
+		 $.ajax({
+				type:'POST',
+				url:ajaxUrl,
+				data: {'pid' : pid,'tab':'Items'},
+				success: function(result){
+					 $('#div1').html(result);
+					 Drupal.attachBehaviors('#div1');
+				},
+				dataType:"json"
+			});
+			
+		});
+	  $('.Metadata').click(function(){
+			 $('#div1').html('loading...');
+			 $.ajax({
+					type:'POST',
+					url:ajaxUrl,
+					data: {'pid' : pid,'tab':'Metadata'},
+					success: function(result){
+						 $('#div1').html(result);
+						 Drupal.attachBehaviors('#div1');
+					},
+					dataType:"json"
+				});
+				
+			});
+	  $('.Description').click(function(){
+		  $('#div1').html('loading...');
+		  $.ajax({
+				type:'POST',
+				url:ajaxUrl,
+				data: {'pid' : pid,'tab':'Description'},
+				success: function(result){
+					 $('#div1').html(result);
+					 Drupal.attachBehaviors('#div1');
+				},
+				dataType:"json"
+			});
+			
+		});
+	  $('.History').click(function(){
+			 $('#div1').html('loading...');
+			 $.ajax({
+					type:'POST',
+					url:ajaxUrl,
+					data: {'pid' : pid,'tab':'History'},
+					success: function(result){
+						 $('#div1').html(result);
+						 Drupal.attachBehaviors('#div1');
+					},
+					dataType:"json"
+				});
+				
+			});
+	 
+})
