4fd347bd9f08ed5dcee2757f38cd48b7dbaeaad4
unknown <bsu@.ornl.gov>
2011-06-27 10:49:41 -0400

    added changes to viewerstab

--- ajaxtabs_atomic.js
+++ ajaxtabs_atomic.js
@@ -5,6 +5,7 @@ $(document).ready(function(){
 	  ajaxUrl=Drupal.settings.basePath;
 	  ajaxUrl=ajaxUrl.concat("ajax/gettabs/atomic");
 	  //alert(ajaxUrl);
+	 $('.Items').css('background-color','#C0C0C0');		 
 	  $('.Items').click(function(){
 		 $('#div1').css('display','block');
 		 $('#viewers').css('display','none');			 
@@ -66,8 +67,7 @@ $(document).ready(function(){
 					 $('.Metadata').css('background-color','#F4F4F4');
 					 $('.Items').css('background-color','#F4F4F4');
 					 $('.History').css('background-color','#F4F4F4');
-					 $('.Viewers').css('background-color','#F4F4F4');
-					 Drupal.attachBehaviors('#div1');
+					 $('.Viewers').css('background-color','#F4F4F4'); Drupal.attachBehaviors('#div1');
 					 $('a.fb_dynamic').live('click',function(e){
 						 e.preventDefault();
 					       $(this).fancybox({  
@@ -89,6 +89,9 @@ $(document).ready(function(){
 			 $('#viewers').css('display','none');			 
 			
 			 $('#div1').html('loading...');
+			  var dWidth  = $(document).width()*.50;
+			  var dHeight = $(document).height()*.75;
+			 
 			 $.ajax({
 					type:'POST',
 					url:ajaxUrl,
@@ -101,6 +104,16 @@ $(document).ready(function(){
 						 $('.Description').css('background-color','#F4F4F4');
 						 $('.Viewers').css('background-color','#F4F4F4');
 						 Drupal.attachBehaviors('#div1');
+						 $('a.fb_dynamic').live('click',function(e){
+						 e.preventDefault();
+					       $(this).fancybox({  
+								'width':dWidth,  
+								'height':dHeight,  
+								'autoScale'         : false,   
+								'type'          : 'iframe'  
+							});
+					       
+						}); 
 						
 					},
 					dataType:"json",
@@ -108,14 +121,38 @@ $(document).ready(function(){
 				
 			});
 	  $('.Viewers').click(function(){
-		  	 $('#div1').css('display','none');
-			 $('#viewers').css('display','block');			 
-			 $('.History').css('background-color','#F4F4F4');							
-			 $('.Metadata').css('background-color','#F4F4F4');
-			 $('.Items').css('background-color','#F4F4F4');
-			 $('.Description').css('background-color','#F4F4F4');
-			 $('.Viewers').css('background-color','#D7D6DB');
-			 Drupal.attachBehaviors('#div1');						
+		 	 $('#div1').css('display','block');
+			 $('#viewers').css('display','none');		 
+			
+			 $('#div1').html('loading...');
+			 $.ajax({
+					type:'POST',
+					url:ajaxUrl,
+					data: {'pid' : pid,'tab':'Viewers'},
+					success: function(result){
+						 $('#div1').html(result);
+						 $('.Viewers').css('background-color','#D7D6DB');
+						 $('.Items').css('background-color','#F4F4F4');
+						 $('.Description').css('background-color','#F4F4F4');
+						 $('.History').css('background-color','#F4F4F4');
+						 $('.Metadata').css('background-color','#F4F4F4');
+						 Drupal.attachBehaviors('#div1');
+						 $('a.fb_dynamic').live('click',function(e){
+							 e.preventDefault();
+						       $(this).fancybox({  
+									'width':dWidth,  
+									'height':dHeight,  
+									'autoScale'         : false,
+									'transitionIn' : 'none',
+									'transitionOut' : 'none',
+									'type'          : 'iframe',  
+								});
+						       
+							}); 
+						},
+					dataType:"json",
+				});
+							
 		});	
 	 
 	 
--- ajaxtabs_compound.js
+++ ajaxtabs_compound.js
@@ -5,7 +5,8 @@ $(document).ready(function(){
 	  ajaxUrl=Drupal.settings.basePath;
 	  ajaxUrl=ajaxUrl.concat("ajax/gettabs/compound");
 	  //alert(ajaxUrl);
-	 $('.Items').click(function(){
+	  $('.Items').click(function(){
+		//alert('click items');
 		 $('#div1').css('display','block');
 		 $('#viewers').css('display','none');			 
 			
@@ -16,18 +17,21 @@ $(document).ready(function(){
 				data: {'pid' : pid,'tab':'Items'},
 				success: function(result){
 					 $('#div1').html(result);
-					 $('.Metadata').css('background-color','#E6E6E6');
-					 $('.Items').css('background-color','#C0C0C0');
-					 $('.Description').css('background-color','#E6E6E6');
-					 $('.History').css('background-color','#E6E6E6');
-					 $('.Viewers').css('background-color','#E6E6E6');
+					 $('.Metadata').css('background-color','#F4F4F4');
+					 $('.Items').css('background-color','#D7D6DB');
+					 $('.Description').css('background-color','#F4F4F4');
+					 $('.History').css('background-color','#F4F4F4');
+					 $('.Viewers').css('background-color','#F4F4F4');
 					 Drupal.attachBehaviors('#div1');
+					 return false;
 				},
 				dataType:"json",
 			});
+			return false;
 			
 		});
 	  $('.Metadata').click(function(){
+			//alert('click metadata');
 		  	 $('#div1').css('display','block');
 			 $('#viewers').css('display','none');		 
 			
@@ -38,18 +42,21 @@ $(document).ready(function(){
 					data: {'pid' : pid,'tab':'Metadata'},
 					success: function(result){
 						 $('#div1').html(result);
-						 $('.Metadata').css('background-color','#C0C0C0');
-						 $('.Items').css('background-color','#E6E6E6');
-						 $('.Description').css('background-color','#E6E6E6');
-						 $('.History').css('background-color','#E6E6E6');
-						 $('.Viewers').css('background-color','#E6E6E6');
+						 $('.Metadata').css('background-color','#D7D6DB');
+						 $('.Items').css('background-color','#F4F4F4');
+						 $('.Description').css('background-color','#F4F4F4');
+						 $('.History').css('background-color','#F4F4F4');
+						 $('.Viewers').css('background-color','#F4F4F4');
 						 Drupal.attachBehaviors('#div1');
+					
 					},
 					dataType:"json",
 				});
 				
+				
 			});
 	  $('.Description').click(function(){
+		//alert('click Description');
 		  $('#div1').css('display','block');
 		 $('#viewers').css('display','none');		 
 			
@@ -62,11 +69,11 @@ $(document).ready(function(){
 				data: {'pid' : pid,'tab':'Description'},
 				success: function(result){
 					 $('#div1').html(result);
-					 $('.Description').css('background-color','#C0C0C0');					 
-					 $('.Metadata').css('background-color','#E6E6E6');
-					 $('.Items').css('background-color','#E6E6E6');
-					 $('.History').css('background-color','#E6E6E6');
-					 $('.Viewers').css('background-color','#E6E6E6');
+					 $('.Description').css('background-color','#D7D6DB');					 
+					 $('.Metadata').css('background-color','#F4F4F4');
+					 $('.Items').css('background-color','#F4F4F4');
+					 $('.History').css('background-color','#F4F4F4');
+					 $('.Viewers').css('background-color','#F4F4F4');
 					 Drupal.attachBehaviors('#div1');
 					 $('a.fb_dynamic').live('click',function(e){
 						 e.preventDefault();
@@ -85,22 +92,36 @@ $(document).ready(function(){
 			
 		});
 	  $('.History').click(function(){
+			//alert('click History');
 		  	 $('#div1').css('display','block');
 			 $('#viewers').css('display','none');			 
 			
 			 $('#div1').html('loading...');
+			  var dWidth  = $(document).width()*.50;
+			  var dHeight = $(document).height()*.75;
+			 
 			 $.ajax({
 					type:'POST',
 					url:ajaxUrl,
 					data: {'pid' : pid,'tab':'History'},
 					success: function(result){
 						 $('#div1').html(result);
-						 $('.History').css('background-color','#C0C0C0');							
-						 $('.Metadata').css('background-color','#E6E6E6');
-						 $('.Items').css('background-color','#E6E6E6');
-						 $('.Description').css('background-color','#E6E6E6');
-						 $('.Viewers').css('background-color','#E6E6E6');
+						 $('.History').css('background-color','#D7D6DB');							
+						 $('.Metadata').css('background-color','#F4F4F4');
+						 $('.Items').css('background-color','#F4F4F4');
+						 $('.Description').css('background-color','#F4F4F4');
+						 $('.Viewers').css('background-color','#F4F4F4');
 						 Drupal.attachBehaviors('#div1');
+						 $('a.fb_dynamic').live('click',function(e){
+							 e.preventDefault();
+						       $(this).fancybox({  
+									'width':dWidth,  
+									'height':dHeight,  
+									'autoScale'         : false,   
+									'type'          : 'iframe'  
+								});
+						       
+							}); 
 						
 					},
 					dataType:"json",
@@ -108,15 +129,42 @@ $(document).ready(function(){
 				
 			});
 	  $('.Viewers').click(function(){
-		  	 $('#div1').css('display','none');
-			 $('#viewers').css('display','block');			 
-			 $('.History').css('background-color','#E6E6E6');							
-			 $('.Metadata').css('background-color','#E6E6E6');
-			 $('.Items').css('background-color','#E6E6E6');
-			 $('.Description').css('background-color','#E6E6E6');
-			 $('.Viewers').css('background-color','#C0C0C0');
-			 Drupal.attachBehaviors('#div1');						
+		 	 $('#div1').css('display','block');
+			 $('#viewers').css('display','none');		 
+			
+			 $('#div1').html('loading...');
+			 var dWidth  = $(document).width()*.70;
+			  var dHeight = $(document).height()*.85;
+			 $.ajax({
+					type:'POST',
+					url:ajaxUrl,
+					data: {'pid' : pid,'tab':'Viewers'},
+					success: function(result){
+						 $('#div1').html(result);
+						 $('.Viewers').css('background-color','#D7D6DB');
+						 $('.Items').css('background-color','#F4F4F4');
+						 $('.Description').css('background-color','#F4F4F4');
+						 $('.History').css('background-color','#F4F4F4');
+						 $('.Metadata').css('background-color','#F4F4F4');
+						 Drupal.attachBehaviors('#div1');
+						 $('a.fb_dynamic').live('click',function(e){
+							 e.preventDefault();
+						       $(this).fancybox({  
+									'width':dWidth,  
+									'height':dHeight,  
+									'autoScale'         : false, 
+									'transitionIn' : 'none',
+									'transitionOut' : 'none',
+									'type'          : 'iframe',  
+								});
+						       
+							}); 
+						},
+					dataType:"json",
+				});
+							
 		});	
+	
 	 
 	 
 })
