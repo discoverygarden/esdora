a0a181f42d6a1d754721f3018178f29de3f3475f
Nigel Banks <nigel.g.banks@gmail.com>
2012-07-24 20:39:40 +0000

    Pretty major refactor to the fedora repository page.

---
+++ esdora/js/esdora.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+  $("div.manage_dataset a").fancybox({
+    overlayShow: true,
+    titlePosition : 'inside',
+    transitionIn : 'none',
+    transitionOut : 'none',
+  });
+});
---
+++ esdora/templates/ManageTab.tpl.php
@@ -0,0 +1,56 @@
+<div class="manage_dataset">
+  <fieldset>
+    <legend>Choose a management option</legend>
+    <ul>
+      <li><a href="#dataset_add_component">Add a component</a></li>
+      <li><a href="#dataset_edit_fgdc">Edit Metadata (FGDC)</a></li>
+      <li><a href="#dataset_add_lineage">Data Lineage</a></li>
+      <li><a href="#dataset_object_details">Object Details</a></li>
+      <li><a href="#add_to_workflow">Add To Workflow</a></li>
+    </ul>
+  </fieldset>
+</div>
+<div style="display: none;">
+  <div id="dataset_add_component" class="esdora-fancy-box-item">
+    <?php print drupal_render($manage_view_add_component); ?>
+    <p/><br/>
+    <a onclick="$.fancybox.close();" href="javascript:;">
+      <center>Close</center>
+    </a>
+  </div>
+  <div id="dataset_edit_fgdc" class="esdora-fancy-box-item" >
+    <?php print drupal_render($manage_view_edit_fgdc); ?>
+    <p/><br/>
+    <a onclick="$.fancybox.close();" href="javascript:;">
+      <center>Close</center>
+    </a>
+  </div>
+  <div id="dataset_edit_dc"  class="esdora-fancy-box-item">
+    <?php print drupal_render($manage_view_edit_dc); ?>
+    <p/><br/>
+    <a onclick="$.fancybox.close();" href="javascript:;">
+      <center>Close</center>
+    </a>
+  </div>
+  <div id="dataset_add_lineage" class="esdora-fancy-box-item">
+    <?php print drupal_render($manage_view_lineage); ?>
+    <p/><br/>
+    <a onclick="$.fancybox.close();" href="javascript:;">
+      <center>Close</center>
+    </a>
+  </div>
+  <div id="dataset_object_details" class="esdora-fancy-box-item">
+    <?php  print drupal_render($object_details); ?>
+    <p/><br/>
+    <a onclick="$.fancybox.close();" href="javascript:;">
+      <center>Close</center>
+    </a>
+  </div>
+  <div id="add_to_workflow" class="esdora-fancy-box-item">
+    <?php  print drupal_render($add_to_workflow); ?>
+    <p/><br/>
+    <a onclick="$.fancybox.close();" href="javascript:;">
+      <center>Close</center>
+    </a>
+  </div>
+</div>
